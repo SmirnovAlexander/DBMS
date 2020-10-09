857201f10aa67647156eb2964dbfc26b4943e0e9
Alexander Smirnov <ru.alexander.smirnov@gmail.com>
2020-10-09 14:02:03 +0300

    finished hw 3

--- ObjectViews/object_views_2.sql
+++ ObjectViews/object_views_2.sql
@@ -67,17 +67,17 @@ drop sequence dept1_seq;
 
 -- create and execute stored procedure that calculates factorial 
 create or replace function fac(a in number)
-return number
-is
-    result number;
-begin
-    if a <= 1 then
-        result := 1;
-    else
-        result := a * fac(a - 1);
-    end if;
-    return result;
-end fac;
+    return number
+    is
+        result number;
+    begin
+        if a <= 1 then
+            result := 1;
+        else
+            result := a * fac(a - 1);
+        end if;
+        return result;
+    end fac;
 /
 
 -- demostrate it works
@@ -85,17 +85,17 @@ select fac(5) from dual;
 
 -- create and execute procedure that accepts employee number and returns number of days he worked
 create or replace function get_work_days(id number)
-return number
-is
-    result number;
-begin
-    select round(((select sysdate from dual) - hiredate), 0) as WORK_DAYS
-    into result
-    from emp
-    where empno = id;
-    
-    return result;
-end get_work_days;
+    return number
+    is
+        result number;
+    begin
+        select round(((select sysdate from dual) - hiredate), 0) as WORK_DAYS
+        into result
+        from emp
+        where empno = id;
+        
+        return result;
+    end get_work_days;
 /
 
 -- demostrate it works
@@ -222,10 +222,50 @@ end;
 -- view entries of `debug_log` after procedure execution
 select * from debug_log;
 
+
+-- create procedure to track dynamical errors
+create or replace procedure log_info
+    (in_info_msg in varchar2,
+     in_source in varchar2
+    )
+    is
+        PRAGMA AUTONOMOUS_TRANSACTION;
+    begin
+        insert into debug_log(id, log_time, message, in_source)
+        values(debug_log_seq.nextval, sysdate, in_info_msg, in_source);
+        commit;
+    exception
+        when others then
+            return;
+    end log_info;
+/
+
+-- create function or procedure that may lead to dynamical error
+create or replace function division(a in number, b in number)
+    return number
+    is
+        result number;
+    begin
+
+        result := a / b;
+        log_info(a || ' / ' || b || ' = ' || result, 'division');
+
+        return result;
+    exception
+        when others then
+            log_info(substr(sqlerrm, 1, 100) || ' (' || a || ' / ' || b || ')', 'division');
+        
+    end division;
+/
+
+-- execute appearence and track of 3 dynamical errors in `debug_log`
+select division(10, 5) from dual;
+select division(25, 5) from dual;
+select division(25, 0) from dual;
+select division(0, 0) from dual;
+select division(-1, 0) from dual;
+select * from debug_log;
+
 -- drop table and sequence
 drop table debug_log;
 drop sequence debug_log_seq;
-
--- create procedure to track dynamical errors
--- create function or procedure that may lead to dynamical error
--- execute appearence and track of 3 dynamical errors in `debug_log`
